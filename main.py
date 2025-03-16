cefa7a7822e4ee48a93e1bcbefd9c7ab5076229e
Anna <ankin0105@yandex.ru>
2025-03-16 15:14:39 +0300

    homework9.1

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
+
+print(get_mask_card_number("0000000000000000"))
+print(get_mask_account("00000000000000000000"))
