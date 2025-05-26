c671b59dfacccf5857d3f7d4d3d97c3e01d24046
Anna <ankin0105@yandex.ru>
2025-05-26 20:23:03 +0300

    Добавлен тест

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,8 +1,20 @@
-from typing import Any
+from typing import Dict
 import pytest
-from unittest.mock import patch, Mock
-from src.external_api import API_KEY, convert_to_rub
+from unittest.mock import patch
+from src.external_api import convert_to_rub
 
+def test_convert_to_rub_success():
+    transaction: Dict = {
+        'operationAmount': {
+            'amount': '100',
+            'currency': {'code': 'USD'}
+        }
+    }
+
+    with patch('requests.get') as mock_get:
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {'result': 7500.50}
+        assert convert_to_rub(transaction) == 7500.50
 
 # Тест для RUB (без обращения к API)
 def test_convert_to_rub_rub_no_api():
