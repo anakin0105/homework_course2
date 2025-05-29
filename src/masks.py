24f5f1a9539713339f227711cd6b89c4322a5c7b
Anna <ankin0105@yandex.ru>
2025-05-29 19:11:41 +0300

    Добавлены логи(masks.log, utils.log).

--- src/masks.py
+++ src/masks.py
@@ -3,31 +3,54 @@ from venv import logger
 
 from src.decorators import log
 import logging
-import datetime
+import os
 
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+log_file = os.path.join(log_dir, "masks.log")
 masks_logger = logging.getLogger("masks_logger")
 masks_logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logs/masks.log", mode="w", encoding="utf-8")
-file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 masks_logger.addHandler(file_handler)
 
 
-@log("log1.txt")
+@log()
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     """Функция принимает на вход номер карты в виде числа и
     возвращает маску номера по правилу XXXX XX** **** XXXX"""
+    masks_logger.info("Запущена функция get_mask_card_number! ")
     if card_number.isdigit() and len(card_number) == 16:
         masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+        masks_logger.info("Функция get_mask_card_number завершила работу! ")
         return masked_number
     else:
+        masks_logger.warning("Входные данные не могут быть обработаны корректно!")
+        masks_logger.info("Функция get_mask_card_number завершила работу! ")
         return "Проверьте правильность введенного номера карты!"
 
 
 @log()
 def get_mask_account(account_number: Union[str]) -> Union[str]:
-    if account_number.isdigit() and len(account_number) == 20:
-        masked_number = "**" + account_number[-4:]
-        return masked_number
-    else:
-        return "Проверьте правильность введенного номера счета!"
+    masks_logger.info("Запущена функция get_mask_account!")
+    try:
+        if account_number.isdigit() and len(account_number) == 20:
+            masked_number = "**" + account_number[-4:]
+            masks_logger.info("Функция get_mask_account завершила работу! ")
+            return masked_number
+        else:
+            masks_logger.warning("Входные данные не могут быть обработаны корректно!")
+            masks_logger.info("Функция get_mask_account завершила работу! ")
+            return "Проверьте правильность введенного номера счета!"
+    except (TypeError, SyntaxError, AttributeError) as e:
+        error_message = f"Функция get_mask_account завершила работу с ошибкой: {e}"
+        masks_logger.error(error_message)  # Логируем ошибку
+        print(error_message)  # Выводим ошибку через print
+        return "Введены некорректные данные!"
+
+
+get_mask_card_number("123")
+get_mask_account("abc")
+print(get_mask_account("12751596837868705199"))
+print(get_mask_account("1596837868705199"))
+print(get_mask_account([]))
--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,19 @@ import os
 from typing import Any
 from typing import Dict
 from typing import List
+import logging
+import os
 
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
+log_file = os.path.join(log_dir, "utils.log")
+masks_logger = logging.getLogger("ulils_logger")
+masks_logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(log_file, mode="w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+masks_logger.addHandler(file_handler)
 
+masks_logger.info("Скрипт запущен")
 def read_transactions_json(file_json) -> List[Dict[Any, Any]]:
     """Читает JSON-файл и возвращает список словарей с данными о транзакция.
     Параметры:
@@ -22,9 +33,14 @@ def read_transactions_json(file_json) -> List[Dict[Any, Any]]:
                 return []
 
             return data
-    except (FileNotFoundError, json.JSONDecodeError, TypeError, KeyError, ValueError):
+    except (FileNotFoundError, json.JSONDecodeError, TypeError, KeyError, ValueError) as e:
+        masks_logger.error(f"Функция get_mask_account завершила работу c ошибкой {e}! ")
         return []
 
 
 file_path = r"C:\Users\Admin\PycharmProjects\pythonProjectBank1\data\operations.json"
+file_path_1 = r"C:\Users\Admin\PycharmProjects\pythonProjectBank1\data\dateerror"
 print(read_transactions_json(file_path))
+print(read_transactions_json([]))
+print(read_transactions_json([23413]))
+print(read_transactions_json(file_path_1))
